ae4a98e14508535e3139a93db09e88462fb7e606
Kostiaffw <kostiantyn.snihyr@pfizer.com>
2022-06-16 15:59:06 +0300

    2. 1. Listing, Search, Filtration.

---
+++ src/components/GalleryList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+      class="card"
+      v-for="picture in pictures"
+      :key="picture.id"
+      style="min-width: 350px; max-width: 350px;"
+  >
+    <img :src="picture.image" class="card-img-top" alt="item.description">
+    <div class="card-body">
+      <h5 class="card-title">title: {{picture.title}}</h5>
+      <p class="card-description">description: {{picture.description}}</p>
+      <p class="card-amount">amount: {{picture.amount}}</p>
+      <p class="card-color">color: {{picture.color}}</p>
+      <p class="card-author">author: {{picture.author.firstName}} {{picture.author.lastName}}</p>
+      <div> tags:
+        <ul class="card-tags">
+          <li
+              v-for="tag in picture.tags"
+              :key="picture.id + tag"
+          >
+            {{ tag }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "GalleryList",
+  props: ["pictures"],
+  components: {
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<style>
+  .card {
+    margin: 20px;
+  }
+</style>
+
---
+++ src/components/SearchPictures.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="col-12 col-md-10 col-lg-7 mx-auto">
+    <div class="input-group my-3">
+      <input
+          id="SearchApts"
+          placeholder="Search"
+          type="text"
+          class="form-control"
+          aria-label="Search Appointments"
+          v-model="searchTerm"
+      >
+      <div class="input-group-append">
+        <button
+            type="button"
+            class="btn btn-primary dropdown-toggle"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+        >
+          Sort by
+          <span class="caret"></span>
+        </button>
+        <div class="dropdown-menu dropdown-menu-right">
+          <a href="#" class="dropdown-item d-flex justify-content-between" id="title"
+             @click="$emit('requestKey', 'title')"
+          >
+            Title
+          </a>
+          <a class="dropdown-item d-flex justify-content-between" href="#" id="author"
+             @click="$emit('requestKey', 'author')"
+          >
+            Author
+          </a>
+          <a href="#" class="dropdown-item d-flex justify-content-between" id="color"
+             @click="$emit('requestKey', 'color')"
+          >
+            Color
+          </a>
+          <div class="dropdown-divider" role="separator"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchAppointments",
+  data() {
+    return {
+      searchTerm: ""
+    };
+  },
+  watch: {
+    searchTerm: function() {
+      this.$emit("searchRecords", this.searchTerm);
+    }
+  },
+  props: ["filterKey"],
+};
+</script>
